1b861cdb138e55e346696544e4b32fe554ffd6b2
Yousaf Wajih <76659264+Yousaf-Wajih@users.noreply.github.com>
2024-06-15 08:29:32 +0500

    Update catppuccin.lua

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,7 +2,7 @@ return {
   -- add gruvbox
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
-  -- Configure LazyVim to load gruvbox
+  -- Configure LazyVim to load catppuccin
   {
     "LazyVim/LazyVim",
     opts = {
